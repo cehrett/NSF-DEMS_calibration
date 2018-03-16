4cdb27a27da9ec239777705610cce0be8d2ab3e4
Carl Ehrett <cehrett@gmail.com>
2018-03-16 16:40:44 -0400

    Code for figures

--- fourth_exam_figures.m
+++ fourth_exam_figures.m
@@ -107,21 +107,39 @@ xlim([0,8000]);
 clc; clear all; close all;
 logit = @(x) log(x./(1-x));
 
-load(['C:\Users\carle\Documents\MATLAB\NSF DEMS\Phase 1\'...
-    '30_MCMCs2.mat']);
+load(['C:\Users\carle\Documents\MATLAB\NSF DEMS\Phase 1\stored_data\'...
+    '30_MCMCs.mat']);
 results = cell(2,1);
 results{1}=all_results2{2}{2};
 results{2}=all_results2{1}{3};
+desired_data = [ results{1}.desired_data ; results{2}.desired_data ];
 
+h = figure('rend','painters','pos',[10 10 1000 275]);
 for jj = 1:2
     bsamps = results{jj}.samples(2002:end,:);
     bsigs = results{jj}.sigma2(2002:end,:);
     
     subplot(1,3,1)
     plot(bsamps(:,1),'ko');
+    title('Volume fraction draws');
+    xlabel('Draw'); ylabel('VF');
+    
+    scale = 0.1;
+    pos = get(gca, 'Position');
+    pos(2) = pos(2)+scale*pos(4);
+    pos(4) = (1-scale)*pos(4);
+    set(gca, 'Position', pos)
     
     subplot(1,3,2)
     plot(bsamps(:,2),'ko');
+    title('Thickness draws');
+    xlabel('Draw'); ylabel('Thickness');
+    
+    scale = 0.1;
+    pos = get(gca, 'Position');
+    pos(2) = pos(2)+scale*pos(4);
+    pos(4) = (1-scale)*pos(4);
+    set(gca, 'Position', pos)
     
     subplot(1,3,3)
     plot(logit(bsamps(:,1)),logit(bsamps(:,2)),'ko');
@@ -131,8 +149,57 @@ for jj = 1:2
     hold on;
     plot(rr(:,1),rr(:,2),'r.');
     hold off;
+    title({'(Logit) VF vs. thickness', 'with proposal covariance'});
+    xlabel('Logit VF'); ylabel('Logit thickness');
+    suptitle(strcat('Desired data: VF',{' '},...
+        num2str(desired_data(jj,1)),{', '},...
+        'Thickness', {' '}, num2str(desired_data(jj,2))));
+    
+    scale = 0.1;
+    pos = get(gca, 'Position');
+    pos(2) = pos(2)+scale*pos(4);
+    pos(4) = (1-scale)*pos(4);
+    set(gca, 'Position', pos)
     
     waitforbuttonpress;
+    %saveas(h,...
+    %    sprintf('FIG%d.png',jj));
 end
 
+
+%% Comparison w/ and w/o boundary constraints
+
+clc; clear all; close all;
+
+load(['C:\Users\carle\Documents\MATLAB\NSF DEMS\Phase 1\stored_data\'...
+   'results_0d_heteroskedPriorObs_hasBndCnds.mat']);
+results1 = results;
+load(['C:\Users\carle\Documents\MATLAB\NSF DEMS\Phase 1\stored_data\'...
+   'results_0d_heteroskedPriorObs.mat']);
+results2 = results;
+
+bsamps1 = results1.samples(2002:end,:);
+bsamps2 = results2.samples(2002:end,:);
+
+h = figure('rend','painters','pos',[10 10 1000 250]);
+subplot(1,2,1);
+acf(bsamps1(:,1),50);
+ylim([-0.1 1]);
+title('Boundary conditions present');
+scale = 0.15;
+pos = get(gca, 'Position');
+pos(2) = pos(2)+scale*pos(4);
+pos(4) = (1-scale)*pos(4);
+set(gca, 'Position', pos)
+subplot(1,2,2);
+acf(bsamps2(:,1),50);
+ylim([-0.1 1]);
+title('Boundary conditions removed')
+scale = 0.15;
+pos = get(gca, 'Position');
+pos(2) = pos(2)+scale*pos(4);
+pos(4) = (1-scale)*pos(4);
+set(gca, 'Position', pos)
+suptitle('Volume fraction autocorrelation');
+saveas(h,sprintf('ACF_bnd_cnds_fig.png'));
 
