b825c30f1fe23939e736c4e9980f159c0382e159
Carl Ehrett <cehrett@gmail.com>
2018-04-20 16:44:38 -0400

    Changed cost grid exploration code to collect and use code uncertainty

--- Cost_grid_workspace.m
+++ Cost_grid_workspace.m
@@ -102,7 +102,7 @@ intervals = zeros(m,3);
 means = zeros(m,3);
 
 % Loop: get the emulator output at each sample drawn in each MCMC
-for ii = 9:m
+for ii = 1:m
     fprintf('Step %d/%d\n',ii,m); % Let us know what step we're on
     
     % Get the outputs for the ii^th MCMC chain
@@ -111,15 +111,19 @@ for ii = 9:m
     % Record them (in a couple places, for convenience)
     % First, all the outputs (one per sample drawn in MCMC)
     outputs{ii} = emout;
-    model_output.by_sample = outputs{ii};
+    model_output.by_sample = emout.output_means;
     % Then the means
-    means(ii,:) = mean(emout);
+    means(ii,:) = mean(emout.output_means);
     model_output.means = means(ii,:);
     % Then the standard deviations
-    intervals(ii,:) = std(emout);
-    model_output.sds = intervals(ii,:);
+    output_gp_sds = emout.output_sds;
+    model_output.sds = output_gp_sds;
     % Now package everything up in the results structs
     results{ii}.model_output = model_output;
+    
+    save([dpath,'stored_data\'...
+    'results_Cost_grid_exploration'],...
+    'results');
     
 end
 
@@ -128,23 +132,28 @@ end
 % well as upper and lower .05 quantiles
 m=size(results,1);
 cost = zeros(m,1);
-pmo = zeros(m,3); % this will store posterior mean emulator outputs
-pso = zeros(m,3); % this, 2 standard deviations
-plo = zeros(m,3); % this, lower alpha quantiles
-puo = zeros(m,3); % this, upper alpha quantiles
-alpha = 0.05; % Set quantile for credible interval
-for ii = 1:m
+cred_level = 90; % Set desired level for credible bands (in %)
+alpha = (100-cred_level)/100; % Convert cred_level to alpha level
+pmo = zeros(m,3); % This will store posterior mean output of emulator
+pdo = zeros(m,3); % ``'' median output
+pso = zeros(m,3); % ``'' appropriate multiple of standard deviations
+plo = zeros(m,3); % ``'' lower (alpha/2) quantile
+puo = zeros(m,3); % ``'' upper (alpha/2) quantile
+for ii = 1:m % This loop populates the above arrays
     pmo(ii,:) = results{ii}.post_mean_out;
-    pso(ii,:) = 2 * results{ii}.model_output.sds;
-    plo(ii,:) = quantile(results{ii}.model_output.by_sample,alpha);
-    puo(ii,:) = quantile(results{ii}.model_output.by_sample,1-alpha);
-    cost(ii)  = results{ii}.desired_obs(3);
+    pdo(ii,:) = quantile(results{ii}.model_output.by_sample,0.5);
+    pso(ii,:) = norminv(1-alpha/2) * mean(results{ii}.model_output.sds);
+    plo(ii,:) = quantile(results{ii}.model_output.by_sample,alpha/2);
+    puo(ii,:) = quantile(results{ii}.model_output.by_sample,1-alpha/2);
+    cost(ii) = results{ii}.desired_obs(3);
 end
-% Here, we just take the above arrays storing means, sds, quantiles and
-% divide them up into one vector for each of the three model outputs.
+% Now we break the arrays up each into 3 vectors, one for each output
 post_cost_mean = pmo(:,3);
 post_defl_mean = pmo(:,1);
 post_rotn_mean = pmo(:,2);
+post_cost_median = pdo(:,3);
+post_defl_median = pdo(:,1);
+post_rotn_median = pdo(:,2);
 post_cost_sd = pso(:,3);
 post_defl_sd = pso(:,1);
 post_rotn_sd = pso(:,2);
@@ -154,6 +163,18 @@ post_defl_lq = plo(:,1);
 post_defl_uq = puo(:,1);
 post_rotn_lq = plo(:,2);
 post_rotn_uq = puo(:,2);
+% Get quantiles plus code uncertainty
+post_cost_uq_cu = post_cost_uq + post_cost_sd;
+post_cost_lq_cu = post_cost_lq - post_cost_sd;
+post_defl_uq_cu = post_defl_uq + post_defl_sd;
+post_defl_lq_cu = post_defl_lq - post_defl_sd;
+post_rotn_uq_cu = post_rotn_uq + post_rotn_sd;
+post_rotn_lq_cu = post_rotn_lq - post_rotn_sd;
+% Get ylims for the two sets of plots
+ylimrat=1.01;
+ylim_cost = [min(post_cost_lq_cu)/ylimrat max(post_cost_uq_cu)*ylimrat];
+ylim_defl = [min(post_defl_lq_cu)/ylimrat max(post_defl_uq_cu)*ylimrat];
+ylim_rotn = [min(post_rotn_lq_cu)/ylimrat max(post_rotn_uq_cu)*ylimrat];
 
 % Begin figures
 h=figure('rend','painters','pos',[10 10 1200 400]);
@@ -175,6 +196,7 @@ plot(cost,post_defl_mean,'or',...
 xl2=xlabel('Target cost');
 ylabel('Deflection');
 xlim([96,350]);
+ylim(ylim_defl);
 
 % Here's plot 2/3
 subplot(1,3,2)
@@ -193,6 +215,7 @@ plot(cost,post_rotn_mean,'or',...
 xl3=xlabel('Target cost');
 ylabel('Rotation');
 xlim([96,350]);
+ylim(ylim_rotn);
 
 % Here's plot 3/3
 subplot(1,3,3)
@@ -213,11 +236,76 @@ plot(cost,post_cost_mean,'or',...
 xl1=xlabel('Target cost');
 ylabel('Observed cost');
 xlim([96,350]);
+ylim(ylim_cost);
 %plot(x,x,'-k','LineWidth',2);
+
+% Save the figure temporarily so we can mess with it later, because
+% suptitle seems to mess things up somehow for making changes after calling
+% it
+savefig(h,'tempfig');
 
 % Now add a main title and fix any infelicities
 suptitle(['Performance metrics vs. (known) target cost,',...
-    ' with upper/lower ',num2str(alpha),' quantile bands']); 
+    ' with ',num2str(cred_level),'% credible interval']); 
+p = get(xl1,'position');
+set(xl1,'position',p + [0 2.75 0]);
+p = get(xl2,'position');
+set(xl2,'position',p + [0 0.00125 0])
+p = get(xl3,'position');
+set(xl3,'position',p + [0 0.0002 0])
+figpos = get(h,'pos');
+
+saveas(h,'FIG_costs.png');
+
+% Now add in code uncertainty. That is, the above assumes that the GP
+% emulator nails the FE code precisely. But of course the GP emulator has
+% nonnegligible variance. That's the code uncertainty. So our confidence
+% bands should reflect it. So we add it in here, by dropping the
+% appropriate multiple of the sd from each lower quantile and adding it to
+% each upper quantile.
+% First, open the figure prior to calling suptitle.
+h=openfig('tempfig');
+subplot(1,3,1);
+pdefluq_code_uncert = pchip(cost,post_defl_uq_cu,x);
+pdefllq_code_uncert = pchip(cost,post_defl_lq_cu,x);
+f=fill([ x , fliplr(x) ], [pdefluq_code_uncert,...
+    fliplr(pdefluq)],'b');
+ff=fill([ x , fliplr(x) ], [pdefllq_code_uncert,...
+    fliplr(pdefllq)],'b');
+set(f,'facealpha',.25);
+set(ff,'facealpha',.25);
+xl2=xlabel('Target cost');
+ylim(ylim_defl);
+
+subplot(1,3,2);
+protnuq_code_uncert = pchip(cost ,post_rotn_uq_cu,x);
+protnlq_code_uncert = pchip(cost ,post_rotn_lq_cu,x);
+f=fill([ x , fliplr(x) ], [protnuq_code_uncert,...
+    fliplr(protnuq)],'b');
+ff=fill([ x , fliplr(x) ], [protnlq_code_uncert,...
+    fliplr(protnlq)],'b');
+set(f,'facealpha',.25);
+set(ff,'facealpha',.25);
+xl3=xlabel('Target cost');
+ylim(ylim_rotn);
+
+subplot(1,3,3);
+pcostuq_code_uncert = pchip(cost ,post_cost_uq_cu,x);
+pcostlq_code_uncert = pchip(cost ,post_cost_lq_cu,x);
+f=fill([ x , fliplr(x) ], [pcostuq_code_uncert,...
+    fliplr(pcostuq)],'b');
+ff=fill([ x , fliplr(x) ], [pcostlq_code_uncert,...
+    fliplr(pcostlq)],'b');
+set(f,'facealpha',.25);
+set(ff,'facealpha',.25);
+xl1=xlabel('Target cost');
+ylim(ylim_cost);
+
+% Now add a main title and fix any infelicities
+suptitle(['Performance metrics vs. (known) target cost,',...
+    ' with ',num2str(cred_level),'% credible interval ',...
+    'including code uncertainty']); 
+set(h,'pos',figpos); % Just so we can reuse the positioning code from above
 p = get(xl1,'position');
 set(xl1,'position',p + [0 2.75 0]);
 p = get(xl2,'position');
@@ -225,4 +313,5 @@ set(xl2,'position',p + [0 0.00125 0])
 p = get(xl3,'position');
 set(xl3,'position',p + [0 0.0002 0])
 
-%saveas(h,'FIG_costs.png');
+saveas(h,'FIG_costs_code_uncert.png');
+
--- Cost_lambda_grid_exploration.m
+++ Cost_lambda_grid_exploration.m
@@ -89,18 +89,19 @@ load([dpath,'stored_data\'...
 % well as upper and lower .05 quantiles
 m=size(results,1); % Store number of target cost_lambdas
 cost_lambda = zeros(m,1); % This will store cost_lambdas
-alpha = 0.05; % Set quantile
+cred_level = 90; % Set desired level for credible bands (in %)
+alpha = (100-cred_level)/100; % Convert cred_level to alpha level
 pmo = zeros(m,3); % This will store posterior mean output of emulator
 pdo = zeros(m,3); % ``'' median output
-pso = zeros(m,3); % ``'' 2 standard deviations
-plo = zeros(m,3); % ``'' lower (alpha) quantile
-puo = zeros(m,3); % ``'' upper (alpha) quantile
+pso = zeros(m,3); % ``'' appropriate multiple of standard deviations
+plo = zeros(m,3); % ``'' lower (alpha/2) quantile
+puo = zeros(m,3); % ``'' upper (alpha/2) quantile
 for ii = 1:m % This loop populates the above arrays
     pmo(ii,:) = results{ii}.post_mean_out;
     pdo(ii,:) = quantile(results{ii}.model_output.by_sample,0.5);
-    pso(ii,:) = 2 * mean(results{ii}.model_output.sds);
-    plo(ii,:) = quantile(results{ii}.model_output.by_sample,alpha);
-    puo(ii,:) = quantile(results{ii}.model_output.by_sample,1-alpha);
+    pso(ii,:) = norminv(1-alpha/2) * mean(results{ii}.model_output.sds);
+    plo(ii,:) = quantile(results{ii}.model_output.by_sample,alpha/2);
+    puo(ii,:) = quantile(results{ii}.model_output.by_sample,1-alpha/2);
     cost_lambda(ii) = results{ii}.Cost_lambda;
 end
 % Now we break the arrays up each into 3 vectors, one for each output
@@ -119,6 +120,18 @@ post_defl_lq = plo(:,1);
 post_defl_uq = puo(:,1);
 post_rotn_lq = plo(:,2);
 post_rotn_uq = puo(:,2);
+% Get quantiles plus code uncertainty
+post_cost_uq_cu = post_cost_uq + post_cost_sd;
+post_cost_lq_cu = post_cost_lq - post_cost_sd;
+post_defl_uq_cu = post_defl_uq + post_defl_sd;
+post_defl_lq_cu = post_defl_lq - post_defl_sd;
+post_rotn_uq_cu = post_rotn_uq + post_rotn_sd;
+post_rotn_lq_cu = post_rotn_lq - post_rotn_sd;
+% Get ylims for the two sets of plots
+ylimrat=1.01;
+ylim_cost = [min(post_cost_lq_cu)/ylimrat max(post_cost_uq_cu)*ylimrat];
+ylim_defl = [min(post_defl_lq_cu)/ylimrat max(post_defl_uq_cu)*ylimrat];
+ylim_rotn = [min(post_rotn_lq_cu)/ylimrat max(post_rotn_uq_cu)*ylimrat];
 
 % Begin the figure
 h=figure('rend','painters','pos',[10 10 1200 400]);
@@ -139,7 +152,7 @@ plot(cost_lambda,post_cost_median,'or',...
 % Plot 2sd errbar
 % errorbar(cost_lambda,post_cost_mean,post_cost_sd,'ob'); 
 xl1=xlabel('\lambda_c_o_s_t');
-
+ylim(ylim_cost);
 ylabel('Cost');
 
 subplot(1,3,2)
@@ -156,6 +169,7 @@ plot(cost_lambda,post_defl_median,'or',...
     x,pdefluq,'-k',...
     x,pdefllq,'-k');
 xl2=xlabel('\lambda_c_o_s_t');
+ylim(ylim_defl);
 ylabel('Deflection');
 
 subplot(1,3,3)
@@ -172,11 +186,76 @@ plot(cost_lambda,post_rotn_median,'or',...
     x,protnuq,'-k',...
     x,protnlq,'-k');
 xl3=xlabel('\lambda_c_o_s_t');
+ylim(ylim_rotn);
 ylabel('Rotation');
+
+% Save the figure temporarily so we can mess with it later, because
+% suptitle seems to mess things up somehow for making changes after calling
+% it
+savefig(h,'tempfig');
 
 % Put a main title over anything, and fix any misplaced labels etc
 suptitle(['Performance metrics vs. \lambda_c_o_s_t,',...
-    ' with upper/lower ',num2str(alpha),' quantile bands']); 
+    ' with ',num2str(cred_level),'% credible interval']); 
+p = get(xl1,'position');
+set(xl1,'position',p + [0 6 0]);
+p = get(xl2,'position');
+set(xl2,'position',p + [0 0.003 0])
+p = get(xl3,'position');
+set(xl3,'position',p + [0 0.00045 0])
+figpos = get(h,'pos');
+
+% Save the figure
+saveas(h,'FIG_cost_lambda.png');
+
+% Now add in code uncertainty. That is, the above assumes that the GP
+% emulator nails the FE code precisely. But of course the GP emulator has
+% nonnegligible variance. That's the code uncertainty. So our confidence
+% bands should reflect it. So we add it in here, by dropping the
+% appropriate multiple of the sd from each lower quantile and adding it to
+% each upper quantile.
+% First, open the figure prior to calling suptitle.
+h=openfig('tempfig');
+subplot(1,3,1);
+pcostuq_code_uncert = pchip(cost_lambda,post_cost_uq_cu,x);
+pcostlq_code_uncert = pchip(cost_lambda,post_cost_lq_cu,x);
+f=fill([ x , fliplr(x) ], [pcostuq_code_uncert,...
+    fliplr(pcostuq)],'b');
+ff=fill([ x , fliplr(x) ], [pcostlq_code_uncert,...
+    fliplr(pcostlq)],'b');
+set(f,'facealpha',.25);
+set(ff,'facealpha',.25);
+ylim(ylim_cost);
+xl1=xlabel('\lambda_c_o_s_t');
+
+subplot(1,3,2);
+pdefluq_code_uncert = pchip(cost_lambda,post_defl_uq_cu,x);
+pdefllq_code_uncert = pchip(cost_lambda,post_defl_lq_cu,x);
+f=fill([ x , fliplr(x) ], [pdefluq_code_uncert,...
+    fliplr(pdefluq)],'b');
+ff=fill([ x , fliplr(x) ], [pdefllq_code_uncert,...
+    fliplr(pdefllq)],'b');
+set(f,'facealpha',.25);
+set(ff,'facealpha',.25);
+ylim(ylim_defl);
+xl2=xlabel('\lambda_c_o_s_t');
+
+subplot(1,3,3);
+protnuq_code_uncert = pchip(cost_lambda,post_rotn_uq_cu,x);
+protnlq_code_uncert = pchip(cost_lambda,post_rotn_lq_cu,x);
+f=fill([ x , fliplr(x) ], [protnuq_code_uncert,...
+    fliplr(protnuq)],'b');
+ff=fill([ x , fliplr(x) ], [protnlq_code_uncert,...
+    fliplr(protnlq)],'b');
+set(f,'facealpha',.25);
+set(ff,'facealpha',.25);
+ylim(ylim_rotn);
+xl3=xlabel('\lambda_c_o_s_t');
+
+suptitle(['Performance metrics vs. \lambda_c_o_s_t,',...
+    ' with ',num2str(cred_level),'% credible interval '...
+    'including code uncertainty']); 
+set(h,'pos',figpos);
 p = get(xl1,'position');
 set(xl1,'position',p + [0 6 0]);
 p = get(xl2,'position');
@@ -184,7 +263,9 @@ set(xl2,'position',p + [0 0.003 0])
 p = get(xl3,'position');
 set(xl3,'position',p + [0 0.00045 0])
 
-%saveas(h,'FIG_cost_lambda.png');
+saveas(h,'FIG_cost_lambda_code_uncert.png');
+
+delete('tempfig.fig');
 
 %% Get confidence intervals at each Cost_lambda
 m=size(results,1); % Get total number of distinct Cost_lambdas
